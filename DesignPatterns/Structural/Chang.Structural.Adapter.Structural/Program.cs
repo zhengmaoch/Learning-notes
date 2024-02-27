e9c46ad0b3097fc630af2d0497b11b3bff6328b3
Chang <zhengmaoch@msn.com>
2024-02-27 14:45:45 +0800

    add code

---
+++ DesignPatterns/Structural/Chang.Structural.Adapter.Structural/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chang.Structural.Adapter.Structural
+{
+    /// <summary>
+    /// Program startup class for Structural
+    /// Adapter Design Pattern.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            // Create adapter and place a request
+            Target target = new Adapter();
+            target.Request();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Target' class
+    /// </summary>
+    class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Adapter' class
+    /// </summary>
+    class Adapter : Target
+    {
+        private Adaptee _adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            // Possibly do some other work
+            //  and then call SpecificRequest
+            _adaptee.SpecificRequest();
+        }
+    }
+
+    /// <summary>
+    /// The 'Adaptee' class
+    /// </summary>
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Chang.Structural.Composite.Structural/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Chang.Structural.Composite.Structural")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chang.Structural.Composite.Structural")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e4b72363-ff50-4a61-bfa5-bd0d2a9d240a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
